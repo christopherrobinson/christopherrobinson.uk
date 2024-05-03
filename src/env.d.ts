f5be7efc5ea79a5d4c4ad5084ce29d1e19a2aa7c
Christopher Robinson <hello@christopherrobinson.uk>
2024-05-03 12:21:06 +0100

    Setup TypeScript IntelliSense for `.env` variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="astro/client" />
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_MAPBOX_ACCESS_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
