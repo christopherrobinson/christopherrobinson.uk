51e783c495628cb1bc19b870832779ad01708d85
Christopher Robinson <hello@christopherrobinson.uk>
2024-05-25 14:29:17 +0100

    Fix typo in `createSlug` comment

--- src/helpers/createSlug.ts
+++ src/helpers/createSlug.ts
@@ -3,6 +3,6 @@ export const createSlug = (title: string): string => (
     .trim()                         // remove leading & trailing whitespace
     .replace(/[^A-Za-z0-9 ]/g, '')  // remove special characters
     .replace(/\s+/g, '-')           // replace spaces
-    .replace(/^-+|-+$/g, '')        // remove leading & trailing separtors
+    .replace(/^-+|-+$/g, '')        // remove leading & trailing separators
     .toLowerCase()                  // output lowercase
 );
