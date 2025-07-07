e6518777a69b7bf718701182db71d53c3fff7ec7
Christopher Robinson <hello@christopherrobinson.uk>
2025-07-07 19:32:45 +0100

    Hardcode API endpoint

--- src/pages/api/mpg/land-rover-freelander-2.png.ts
+++ src/pages/api/mpg/land-rover-freelander-2.png.ts
@@ -4,8 +4,8 @@ import path from 'path';
 
 export const prerender = false;
 
-export const GET: APIRoute = async ({ params }) => {
-  const vehicleSlug = params.vehicle;
+export const GET: APIRoute = async () => {
+  let vehicleSlug = 'land-rover-freelander-2';
 
   if (!vehicleSlug) {
     return new Response('Missing vehicle.', { status: 400 });
@@ -29,11 +29,11 @@ export const GET: APIRoute = async ({ params }) => {
     const imagePath = path.resolve(`public/images/vehicles/${vehicleSlug}.jpg`);
     const buffer = await generateMpgImage(((totalMiles / totalLitres) * 4.544), imagePath);
 
-    return new Response(buffer, {
+    return new Response(Uint8Array.from(buffer), {
       status: 200,
       headers: {
         'Content-Type': 'image/png',
-        'Content-Disposition': 'inline; filename="mpg.png"',
+        'Content-Disposition': `inline; filename="${vehicleSlug}.png"`,
       },
     });
   }
